fd489a2009dc37be63de34deeb934955cbd657cf
Mahdi <mahdibagherichanel@gmail.com>
2025-07-28 19:50:25 +0330

    update project with new fucher

---
+++ app/privacy/page.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="max-w-3xl mx-auto p-6 space-y-6 font-sans text-gray-900">
+      <h1 className="text-3xl font-bold text-center">Privacy Policy</h1>
+
+      <section className="space-y-3">
+        <p>
+          This is Color Hunt is Privacy Policy for www.colorhunt.co. This
+          document explains Color Hunt policies for the collection, use, and
+          disclosure of personal information on www.colorhunt.co. This privacy
+          policy deals with personally-identifiable information (referred to as
+          data below) that may be collected by this site.
+        </p>
+      </section>
+
+      <section className="space-y-3">
+        <h2 className="text-2xl font-semibold">COLLECTION OF DATA</h2>
+        <p>
+          As on many websites, the site editor may automatically receive general
+          information that is contained in server log files, such as your IP
+          address, and cookie information. Information about how advertising may
+          be served on this site (if it is indeed the site editor is policy to
+          display advertising) is set forth below.
+        </p>
+      </section>
+
+      <section className="space-y-3">
+        <h2 className="text-2xl font-semibold">USE OF DATA</h2>
+        <p>
+          Data may be used to customize and improve your user experience on this
+          site. Efforts will be made to prevent your data from being made
+          available to third parties unless:
+        </p>
+        <ul className="list-disc list-inside space-y-1">
+          <li>provided for otherwise in this Privacy Policy;</li>
+          <li>
+            your consent is obtained, such as when you choose to opt-in or
+            opt-out for the sharing of data;
+          </li>
+          <li>
+            a service provided on our site requires interaction with a third
+            party, or is provided by a third party, such as an application
+            service provider;
+          </li>
+          <li>pursuant to legal action or law enforcement;</li>
+          <li>
+            it is found that your use of this site violates the site editor is
+            policy, terms of service, or other usage guidelines, or if it is
+            deemed reasonably necessary by the site editor to protect the site
+            editor is legal rights and/or property; or
+          </li>
+          <li>
+            this site is purchased by a third party, in which case that third
+            party will be able to use the data in the same manner as set forth
+            in this policy.
+          </li>
+        </ul>
+        <p>
+          In the event you choose to use links displayed on this website to
+          visit other websites, you are advised to read the privacy policies
+          published on those sites. Color Hunt uses third-party advertising
+          companies to serve ads when you visit our website. These companies may
+          use information (not including your name, address, email address, or
+          telephone number) about your visits to this and other websites in
+          order to provide advertisements about goods and services of interest
+          to you.
+        </p>
+      </section>
+
+      <section className="space-y-3">
+        <h2 className="text-2xl font-semibold">COOKIES</h2>
+        <p>
+          Like many websites, this website sets and uses cookies to enhance your
+          user experience — to remember your personal settings, for instance.
+          Advertisements may display on this website and, if so, may set and
+          access cookies on your computer; such cookies are subject to the
+          privacy policy of the parties providing the advertisement. However,
+          the parties providing the advertising do not have access to this site
+          s cookies. These parties usually use non-personally-identifiable or
+          anonymous codes to obtain information about your visits to this site.
+        </p>
+        <p>
+          Please note that turning off advertising cookies won’t mean that you
+          are not served any advertising, merely that it will not be tailored to
+          your interests.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default Page;
