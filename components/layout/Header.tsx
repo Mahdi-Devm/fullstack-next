58ca0b8b13a6e575adc24a012ab79227eca51022
Mahdi <mahdibagherichanel@gmail.com>
2025-07-28 19:50:37 +0330

    update project with new fucher

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import React from "react";
-import { TfiMore } from "react-icons/tfi";
 import { CiSearch } from "react-icons/ci";
 import Borderb from "../ui/Borderb";
+import DropdownListHeader from "../ui/DropdownListHeader";
 
 function Header() {
   return (
     <>
       <header className="w-full flex items-center justify-between p-3 gap-5">
-        <div className="w-full sm:w-[300px] flex items-center justify-center">
+        <div className="w-[100px] sm:w-[300px] flex items-center justify-center">
           <Image
-            width={120}
+            width={50}
             height={100}
             src="/logo/logomobil.png"
             alt="mobile logo"
@@ -30,7 +30,7 @@ function Header() {
           <input
             type="search"
             placeholder="search palettes"
-            className="pl-10 w-full h-8 rounded-[20px] focus:outline-none"
+            className="pl-10 w-50 h-8 rounded-[20px] focus:outline-none"
           />
           <CiSearch
             className="absolute left-3 top-1/2 -translate-y-1/2 text-black pointer-events-none"
@@ -38,12 +38,12 @@ function Header() {
           />
         </div>
 
-        <div className="w-[500px] flex items-center justify-between gap-6">
+        <div className="w-[100px] md:w-[500px] flex items-center justify-between gap-6">
           <div className="border border-gray-300 items-center rounded-xl hidden md:flex">
             <Image src="/icon/chrome.png" alt="chrome" width={40} height={40} />
             <button className="p-2 rounded-md">Add to Chrome</button>
           </div>
-          <TfiMore size={30} className="text-sm" />
+          <DropdownListHeader />
         </div>
       </header>
 
